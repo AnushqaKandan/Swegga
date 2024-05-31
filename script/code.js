5efb6a6124ceeb783604dff4054fa4762ba00a25
Name <anushqakandan03@gmail.com>
2024-05-31 11:11:34 +0200

    Edits

--- script/code.js
+++ script/code.js
@@ -19,10 +19,8 @@ function calculation() {
    let total2 = eval(mPrice.innerText * mQnty.value)
    let total3 = total1 + total2
 
-   fTotal.innerText = total1
-   mTotal.innerText = total2
+   fTotal.innerText = total1.toFixed(2)
+   mTotal.innerText = total2.toFixed(2)
    Total.innerText = total3.toFixed(2)
-   
-   
    
 }
